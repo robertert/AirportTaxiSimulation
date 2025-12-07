1495204ed2d55c090d602244ad1b46158e575ebb
robertert <jacakrobert33@gmail.com>
2025-12-07 11:28:38 +0100

    solara added

---
+++ run_solara.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Uruchomienie wizualizacji Solara
+
+echo "🚀 Uruchamianie interaktywnej wizualizacji Solara..."
+echo "📱 Otwórz przeglądarkę na http://localhost:8765"
+echo ""
+
+solara run solara_app.py
+
